d1c4d529442914c3fcb06cac0858708486590d9a
Kevin Hastlehoff <56676343+Oyopiz@users.noreply.github.com>
2023-01-01 01:29:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,63 @@
+import os
+import PySimpleGUI as sg
+import youtube_dl
 import threading
-from tkinter.messagebox import showerror, showwarning
-
-from Tools.scripts.combinerefs import combine
-from pytube import Playlist
-
-from tkinter import *
-
-root = Tk()
-root.geometry("600x400")
-root.title("Youtube Playlist Downloader")
-input_variable = StringVar()
 
 
-def audio():
-    if input_variable.get() == "":
-        showwarning('No playlist detected', 'Please add a playlist url')
-    else:
-        p = Playlist(input_variable.get())
-    lbl2 = Label(root, text=f'Downloading: {p.title}', fg="blue", width=80, font="courrier")
-    lbl2.place(x=100, y=300)
-    for video in p.videos:
-        print(video.title)
-        st = video.streams.get_highest_resolution()
-        st.download()
-        # video.streams.first().download()
 
 
-def sel():
-    selection = str(var.get())
-    print(selection)
+# Download function
+def download(ydl_opts, url):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    # Show a success message
+    sg.popup("Playlist downloaded!")
+# Set up the UI layout
+layout = [
+    [sg.Text("Enter a playlist URL:")],
+    [sg.Input(key="url")],
+    [sg.Text("Enter the download path:")],
+    [sg.Input(key="path"), sg.FolderBrowse()],
+    [sg.Button("Download"), sg.Cancel()],
+    [sg.Text("Items downloaded:", size=(15, 1)), sg.Text(key="items", size=(5, 1))]
+]
+
+# Create the window
+window = sg.Window("Playlist Downloader", layout)
+
+# Event loop
+while True:
+    event, values = window.read()
+
+    # If the user clicks the "Download" button
+    if event == "Download":
+        url = values["url"]
+        path = values["path"]
+
+        # Set the download options
+        ydl_opts = {
+            "outtmpl": os.path.join(path, "%(title)s.%(ext)s"),
+            "format": "bestaudio/best",
+            "postprocessors": [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }],
+
+        }
+
+        # Create a thread to download the playlist
+        thread = threading.Thread(target=download, args=(ydl_opts, url))
+        thread.start()
+
+    # If the user clicks the "Cancel" button or closes the window
+    if event in (sg.WIN_CLOSED, "Cancel"):
+        break
+
+# Clean up
+window.close()
 
 
-var = IntVar()
-R1 = Radiobutton(root, text="Option 1", variable=var, value=1,
-                 command=sel)
-R2 = Radiobutton(root, text="Option 2", variable=var, value=2,
-                 command=sel)
-R3 = Radiobutton(root, text="Option 3", variable=var, value=3,
-                 command=sel)
-stop_event = threading.Event()
+# Progress hook function
 
-
-def video():
-    if input_variable.get() == "":
-        showwarning('No playlist detected', 'Please add a playlist url')
-    else:
-        p = Playlist(input_variable.get())
-        lbl2 = Label(root, text=f'Downloading: {p.title}', fg="blue", width=70, font="courrier")
-        lbl2.place(x=100, y=250)
-    for video in p.videos:
-        print(video.title)
-        st = video.streams.get_highest_resolution()
-        st.download()
-        # video.streams.first().download()
-
-
-entry1 = Entry(root, fg="blue", textvariable=input_variable, width=80)
-lbl1 = Label(root, text="Enter playlist URL", fg="blue", font="courrier")
-lbl1.place(x=120, y=50)
-button = Button(root, text="Download Audios", command=threading.Thread(target=audio).start, bg="cyan4", fg="white",
-                font="courrier")
-button1 = Button(root, text="Download Videos", command=threading.Thread(target=video).start, bg="cyan4", fg="white",
-                 font="courrier")
-entry1.place(x=100, y=100)
-button.place(x=300, y=200)
-button1.place(x=100, y=200)
-root.mainloop()
